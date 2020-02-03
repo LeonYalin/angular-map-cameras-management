2364b3c4cf73ad24942a9b3a45ca17820b387009
Leon Yalin <yelinichevl@gmail.com>
2020-02-03 20:09:55 +0200

    fix for build --prod #3

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/app'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/app/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
